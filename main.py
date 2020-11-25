da51bed27e53ab522c4b9410565039107ae006c0
Ilya Goldobin <ilyxenc@xryunya.local>
2020-11-25 11:27:05 +0300

    Updating main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import functions as fn
 
 # выбор случайного числа
-p = fn.randPrime(3)
+p = fn.randPrime(10)
 
 # обмен сообщениями трёхэтапным протоколом Шамира
 fn.messageTransmission(p, 'Hello')
